9165c507cee2c481e7cc17fabddfb846b9a99720
Laperavee <tomlebonpro@gmail.com>
2024-02-22 10:30:16 +0100

    Add import_passwords_csv

--- home/views.py
+++ home/views.py
@@ -27,6 +27,30 @@ def edit_password(request, password_id):
         form = PasswordForm(instance=password_instance)
 
     return render(request, "pages/edit_password.html", {'form': form, 'password': password_instance})
+def import_passwords_csv(request):
+    if request.method == 'POST' and request.FILES['csv_file']:
+        csv_file = request.FILES['csv_file']
+
+        # Ensure the uploaded file is a CSV file
+        if not csv_file.name.endswith('.csv'):
+            return render(request, 'error.html', {'error': 'File is not a CSV'})
+
+        # Process the CSV file
+        decoded_file = csv_file.read().decode('utf-8').splitlines()
+        csv_reader = csv.reader(decoded_file)
+        next(csv_reader)  # Skip the header row
+
+        for row in csv_reader:
+            _, created = Password.objects.get_or_create(
+                site=row[0],
+                name_site=row[1],
+                username=row[2],
+                password=row[3]
+            )
+
+        return render(request, 'pages/success.html', {'message': 'Passwords imported successfully'})
+
+    return render(request, 'pages/import_passwords.html')
 def delete_password(request, password_id):
     password_instance = get_object_or_404(Password, id=password_id)
 
