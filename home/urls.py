d8abcc5ee1e08539bde0d817098490ecefa0d295
Laperavee <tomlebonpro@gmail.com>
2024-02-22 10:31:15 +0100

    Add import_passwords_csv template and url

--- home/urls.py
+++ home/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
   path('edit_password/<int:password_id>/', views.edit_password, name='edit_password'),
   path('delete_password/<int:password_id>/', views.delete_password, name='delete_password'),
   path('export_passwords_csv/', views.export_passwords_csv, name='export_passwords_csv'),
-
+  path('import_passwords_csv/', views.import_passwords_csv, name='import_passwords_csv'),
 ]
 
