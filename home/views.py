f2bdf7809d9f1f8ba09381718411a0a3fd954734
Laperavee <tomlebonpro@gmail.com>
2024-02-22 10:16:07 +0100

    Add Password view

--- home/views.py
+++ home/views.py
@@ -5,3 +5,16 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PasswordForm
 from .models import Password
+def password(request):
+    if request.method == "POST":
+        form = PasswordForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = PasswordForm()
+
+    return render(request, "pages/password_form.html", {'form': form})
+def tables(request):
+  passwords = Password.objects.all()
+  return render(request, "pages/index.html", {"passwords" : passwords})
